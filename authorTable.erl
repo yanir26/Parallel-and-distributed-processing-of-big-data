3873ca964354c70469c2160740803eb24d4abcb3
yanir26 <62119972+yanir26@users.noreply.github.com>
2021-08-11 07:58:09 +0300

    Add files via upload

--- authorTable.erl
+++ authorTable.erl
@@ -2,14 +2,13 @@
 -module(authorTable).
 
 %% API
--export([main/0]).
+-export([makeTable/2]).
 -include_lib("wx/include/wx.hrl").
 -define(DEPTH,4).
 
-main()->
-  Input = "a",
+makeTable(Input,Edges)->
   G = digraph:new(),
-  buildTree([{"a","b"},{"a","c"},{"b","d"},{"d","f"}],G),
+  buildTree(Edges,G),
   Ets = ets:new(tab,[set]),
   buildEts(Ets,1),
   updateEts(G,1,[Input],Input,Ets),
@@ -57,7 +56,6 @@ buildEts(Ets,Level)->
   [ets:insert(Ets,{{A,Level},0})||A<-["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]],
   buildEts(Ets,Level+1).
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%The func get a list of edge and build digraph:
 buildTree(Edg,build)->
   G = digraph:new(),
@@ -70,5 +68,3 @@ buildTree(Edg,G)->
   digraph:add_vertex(G,element(2,hd(Edg))),
   digraph:add_edge(G,element(1,hd(Edg)),element(2,hd(Edg))),
   buildTree(tl(Edg),G).
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
